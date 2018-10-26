fd281d64c8181f289da6a958a15c1456119119b9
Lauren Reilly <LaurenReillyNet@gmail.com>
2018-10-26 13:08:41 -0400

    completed render students

--- renderNametags.js
+++ renderNametags.js
@@ -3,10 +3,10 @@ function renderNametags(nametags) {
     var nametagHTML = nametags.map(function(name) {
         return `
         <div class="d-flex flex-column border border-dark rounded mt-5" style="margin: 0 auto; width: 402px">
-            <div class="border rounded" style="background-color: blue; width: 400px; height: 50px">
+            <div class="border rounded-top" style="background-color: blue; width: 400px; height: 50px">
                 <h3 style="color: white;" class="text-center">Hello my name is:</h3>
             </div>
-            <div class="border rounded" style="width: 400px; height: 150px;">
+            <div class="border rounded-bottom" style="width: 400px; height: 150px;">
                 <h2 class="text-center mt-5">${name}</h2>
             </div>
         </div>
--- renderStudents.js
+++ renderStudents.js
@@ -1,10 +1,19 @@
 
 function renderStudents(students) {
-    return `
-        <div class="text-center mt-5">
-            <code>${JSON.stringify(students)}</code>
+    var mainContainer = `
+        <div class="d-flex flex-column align-items-center border border-dark mt-5" style="width: 402px; margin: 0 auto">
+            <h2 class="text-center">Role Call: </h2>
         </div>
     `
+    var studentStatus = students.map(function (student) {
+       return `
+        <div class="${student.isPresent} text-center border border-dark" style="width: 400px; height: 50px; margin: 0 auto;">
+            ${student.name}
+        </div>
+    `
+    });
+
+    return mainContainer += studentStatus.join("");
 }
 
 function students() {
